bb5a205b743d71907806d41724caa828e69e9a67
Adrian <adrianbravo145@gmail.com>
2021-10-16 15:45:58 -0300

    controller user

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -4,8 +4,14 @@ import validator from "validator";
 import { Usuario } from "../models/userModel";
 import Token from "../utils/token";
 import { verificaToken } from "../middlewares/auth";
+import fileUpload from "express-fileupload";
+import express from "express";
+import uniqid from "uniqid";
 
 const userRoutes = Router();
+const app = express();
+app.use(fileUpload());
+
 
 userRoutes.get("/user", async (req: any, res: Response) => {
   try {
@@ -27,10 +33,7 @@ userRoutes.get("/user", async (req: any, res: Response) => {
   }
 });
 
-userRoutes.post(
-  "/create-user",
-  [verificaToken],
-  async (req: Request, res: Response) => {
+userRoutes.post("/create-user", [], async (req: Request, res: Response) => {
     try {
       const { name, email, password } = req.body;
       if (
@@ -43,6 +46,15 @@ userRoutes.post(
           email,
           password: bcrypt.hashSync(password, 10),
         };
+
+        const userEmailExist = await Usuario.findOne({ email: email }).exec();
+        if (userEmailExist) {
+          return res.status(400).json({
+            ok: false,
+            message: `El usuario con el correo ${email} ya existe en el sistema`
+          });
+        }
+
         const userCreated = await Usuario.create(user);
         return res.status(201).json({
           ok: true,
@@ -107,6 +119,100 @@ userRoutes.post("/login", async (req: Request, res: Response) => {
       errors: {
         message: "Error no coincide El usuario registrado en la base de datos",
       },
+    });
+  }
+});
+
+userRoutes.put("/update-user/:id", async (req: any, res: Response) => {
+  const { id } = req.params;
+  try {
+    const { name, email } = req.body;
+    const userUpdate: any = await Usuario.findById(id).exec();
+    if (userUpdate) {
+      if (!validator.isEmpty(name)) {
+        userUpdate.name = name || "";
+        userUpdate.email = email || "";
+        const userSaved = await userUpdate.save();
+        return res.status(200).json({
+          ok: true,
+          mensaje: "Usuario Actualizado exitosamente",
+          userSaved,
+        });
+      } else {
+        return res.status(400).json({
+          ok: false,
+          message: "Los datos no son validos",
+          error: {
+            errors: {
+              message: "Se debe ingresar al menos el nombre del usuario.",
+            },
+          },
+        });
+      }
+    } else {
+      return res.status(400).json({
+        ok: true,
+        mensaje: "El usuario con el " + id + "no existe",
+        errors: { message: "No existe un usuario con ese ID" },
+      });
+    }
+  } catch (error) {
+    return res.status(500).json({
+      ok: false,
+      mensaje: "El usuario con el id " + id + " no existe",
+      errors: { message: "No existe un usuario con ese ID" },
+    });
+  }
+    
+});
+
+
+userRoutes.delete("/delete-user/:id", async (req: any, res: Response) => {
+  const { id } = req.params;
+  try {
+    const userDeleted: any = await Usuario.findByIdAndRemove(id).exec();
+    if (userDeleted) {
+      res.status(204).json({
+        ok: true,
+        mensaje: "Pc eiminado exitosamente",
+        userDeleted,
+      });
+      return [];
+    } else {
+      return res.status(400).json({
+        ok: false,
+        mensaje: "El usuario con el " + id + " ya no existe",
+        errors: { message: "No existe una usuario con ese ID" },
+        user: []
+      });
+    }
+  } catch (error) {
+    return res.status(500).json({
+      ok: false,
+      mensaje: "El usuario con el id " + id + " no existe",
+      errors: { message: "No existe un usuario con ese ID" },
+      error,
+    });
+  }
+});
+
+userRoutes.put("/upload-img/:id", async (req: any, res: Response) => {
+  const { id } = req.params;
+  const files = req.files;
+  console.log({req:req.files, id})
+  try {
+    if (!req.files || Object.keys(req.files).length === 0) {
+      return res.status(400).json({
+        ok: false,
+        mensaje: "No selecciono ningun archivo",
+        errors: { message: "Debe seleccionar una imagen" },
+      });
+    }
+   
+  } catch (error) {
+    return res.status(500).json({
+      ok: false,
+      error,
     });
   }
 });
